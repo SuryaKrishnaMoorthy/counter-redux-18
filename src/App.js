5af1d01e1818425d09d58b21580f79bb1ceff980
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-10-28 09:35:00 -0400

    add toggling data to redux satte

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Auth from './components/Auth';
+// import Auth from './components/Auth';
 import Counter from './components/Counter';
-import Header from './components/Header';
-import UserProfile from './components/UserProfile';
+// import Header from './components/Header';
+// import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <React.Fragment>
-      <Header />
+      {/* <Header /> */}
       <Counter />
-      <UserProfile />
-      <Auth />
+      {/* <UserProfile /> */}
+      {/* <Auth /> */}
     </React.Fragment>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state={counter: 0}, action) => {
+const counterReducer = (state={ counter: 0, showCounter: true }, action) => {
 	switch(action.type) {
 		case 'increment':
-			return {counter: state.counter + 1};
+			return { counter: state.counter + 1, showCounter: state.showCounter };
+		case 'increase':
+			return { counter: state.counter + action.payload.amount, showCounter: state.showCounter };
 		case 'decrement':
-			return { counter: state.counter - 1};
+			return { counter: state.counter - 1, showCounter: state.showCounter };
+		case 'toggle':
+			return { showCounter: !state.showCounter, counter: state.counter };
 		default:
 			return state;
 	}
